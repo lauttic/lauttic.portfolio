e7d8349dd49759522193da68b2be3e2504221970
Lautaro Corceiro <lautaro.corceiro@megatrans.com.ar>
2024-09-10 16:35:13 -0300

    update4

--- docs/app/app-routing.module.ts
+++ docs/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-
+    {path: '', component: AppComponent}
 ];
 
 @NgModule({
